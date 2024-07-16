c1a5e3a70b7f8b0ff7a1c0b6adab880882d0c9f2
d4Jt <jsmsj04@gmail.com>
2024-07-17 02:50:29 +0700

    Update CORS

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,50 +1,49 @@
-import React, { createContext, useReducer, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { createContext, useReducer, useEffect, useState } from 'react'
+import axios from 'axios'
 
-import { authReducer } from 'reducers/authReducer';
-import { API_ROOT } from 'utilities/constants';
-import setAuthToken from 'utilities/setAuthToken';
+import { authReducer } from 'reducers/authReducer'
+import { API_ROOT } from 'utilities/constants'
+import setAuthToken from 'utilities/setAuthToken'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 const AuthContextProvider = ({ children }) => {
    const [authState, dispatch] = useReducer(authReducer, {
       authLoading: true,
       isAuthenticated: false,
       user: null,
-   });
+   })
 
    // Authenticate user
    const loadUser = async () => {
       if (localStorage['wfmg-login']) {
-         setAuthToken(localStorage['wfmg-login']);
+         setAuthToken(localStorage['wfmg-login'])
       }
 
       try {
          const response = await axios.get(`${API_ROOT}/v1/auth`, {
             withCredentials: true,
-            credentials: 'include',
-         });
+         })
          if (response.data.success) {
             dispatch({
                type: 'SET_AUTH',
                payload: { isAuthenticated: true, user: response.data.user },
-            });
+            })
          }
       } catch (err) {
-         localStorage.removeItem('wfmg-login');
-         setAuthToken(null);
+         localStorage.removeItem('wfmg-login')
+         setAuthToken(null)
          dispatch({
             type: 'SET_AUTH',
             payload: { isAuthenticated: false, user: null },
-         });
+         })
       }
-   };
+   }
 
    useEffect(() => {
-      const loadingUser = async () => await loadUser();
-      loadingUser();
-   }, []);
+      const loadingUser = async () => await loadUser()
+      loadingUser()
+   }, [])
 
    // Login
    const loginUser = async (userForm) => {
@@ -54,20 +53,19 @@ const AuthContextProvider = ({ children }) => {
             userForm,
             {
                withCredentials: true,
-               credentials: 'include',
             }
-         );
+         )
          if (response.data.success) {
-            localStorage.setItem('wfmg-login', response.data.accessToken);
+            localStorage.setItem('wfmg-login', response.data.accessToken)
          }
-         await loadUser();
+         await loadUser()
 
-         return response.data;
+         return response.data
       } catch (error) {
-         if (error.response.data) return error.response.data;
-         else return { success: false, message: error.message };
+         if (error.response.data) return error.response.data
+         else return { success: false, message: error.message }
       }
-   };
+   }
 
    // Register
    const registerUser = async (userForm) => {
@@ -77,38 +75,37 @@ const AuthContextProvider = ({ children }) => {
             userForm,
             {
                withCredentials: true,
-               credentials: 'include',
             }
-         );
+         )
          if (response.data.success) {
-            localStorage.setItem('wfmg-login', response.data.accessToken);
+            localStorage.setItem('wfmg-login', response.data.accessToken)
          }
 
-         await loadUser();
+         await loadUser()
 
-         return response.data;
+         return response.data
       } catch (error) {
-         if (error.response.data) return error.response.data;
-         else return { success: false, message: error.message };
+         if (error.response.data) return error.response.data
+         else return { success: false, message: error.message }
       }
-   };
+   }
 
    // Logout
    const logoutUser = () => {
-      localStorage.removeItem('wfmg-login');
+      localStorage.removeItem('wfmg-login')
       dispatch({
          type: 'SET_AUTH',
          payload: { isAuthenticated: false, user: null },
-      });
-   };
+      })
+   }
 
    // Context data
-   const authContextData = { loginUser, registerUser, logoutUser, authState };
+   const authContextData = { loginUser, registerUser, logoutUser, authState }
    return (
       <AuthContext.Provider value={authContextData}>
          {children}
       </AuthContext.Provider>
-   );
-};
+   )
+}
 
-export default AuthContextProvider;
+export default AuthContextProvider

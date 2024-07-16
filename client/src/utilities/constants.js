cf7b986c3a1c987b48634c1e07124e417f4bcf52
Đạt <jsmsj04@gmail.com>
2024-07-17 01:14:00 +0700

    Update constants.js

--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -3,7 +3,6 @@
  * on 28-01-2023
  */
 export const API_ROOT = 'https://wfmg.onrender.com';
-
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE';
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM';
 
